fd10be8d61c86567fb357ad85af8f076fdee1c47
5pponent <ahdwjdtprtm@gmail.com>
2023-01-02 15:57:18 +0900

    feat: Post 모델 추가 및 데이터베이스 migrate 수행

--- prac/blog/models.py
+++ prac/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+'''
+모델 Post: django.db의 models.Model을 상속받음, JPA 엔티티와 유사
+- author: 
+- title: 
+- text: 
+- created_date: 
+- published_date: 
+'''
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
