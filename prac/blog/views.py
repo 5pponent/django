72c65d233e5282eddd0df58a2bb4bc27362a2b26
5pponent <ahdwjdtprtm@gmail.com>
2023-01-02 19:01:04 +0900

    feat: Add Post form

--- prac/blog/views.py
+++ prac/blog/views.py
@@ -1,7 +1,7 @@
-from http.client import HTTPResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -13,5 +13,10 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    
+
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
