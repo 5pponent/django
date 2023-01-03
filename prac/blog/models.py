69e6b0fdcd792c8a53367577730268866e703989
5pponent <ahdwjdtprtm@gmail.com>
2023-01-03 11:21:01 +0900

    feat: Post Update 기능 추가

--- prac/blog/models.py
+++ prac/blog/models.py
@@ -4,14 +4,14 @@ from django.utils import timezone
 
 # Create your models here.
 
-'''
+"""
 모델 Post: django.db의 models.Model을 상속받음, JPA 엔티티와 유사
 - author: 
 - title: 
 - text: 
 - created_date: 
 - published_date: 
-'''
+"""
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
--- prac/blog/views.py
+++ prac/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -7,6 +7,7 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.order_by('-created_date')
+    Post.objects.order_by('-created_date')
 
     return render(request, './blog/post_list.html', {'posts': posts})
 
@@ -16,7 +17,32 @@ def post_detail(request, pk):
 
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    
     return render(request, 'blog/post_edit.html', {'form': form})
